a6a36b2739c3fc6b829c490c273e1364cf7b0fbf
Marcos Ledesma <marcos.ledesma.picazo@cieep.com>
2020-12-21 17:50:56 +0100

    Implementado FireBase

--- app/src/main/java/com/marcosledesma/agendacontactosedu/MainActivity.java
+++ app/src/main/java/com/marcosledesma/agendacontactosedu/MainActivity.java
@@ -6,12 +6,19 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.marcosledesma.agendacontactosedu.adapters.ContactosAdapter;
 import com.marcosledesma.agendacontactosedu.configuraciones.Configuracion;
 import com.marcosledesma.agendacontactosedu.modelos.Contacto;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -37,9 +44,14 @@ public class MainActivity extends AppCompatActivity {
     // 3. Elemento a replicar
     private int resource;
 
+    // Comentamos todo lo relacionado con las sp para utilizar FireBase
     // Shared Preferences (almacenamiento persistente)
-    private SharedPreferences sharedPreferences;
-    private Gson parser;
+    // private SharedPreferences sharedPreferences;
+    // private Gson parser;
+
+    // BD y referencia a FireBase
+    private FirebaseDatabase database;
+    private DatabaseReference refContactos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,19 +60,54 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        sharedPreferences = getSharedPreferences(Configuracion.SP_LISTA, MODE_PRIVATE);
+        // sharedPreferences = getSharedPreferences(Configuracion.SP_LISTA, MODE_PRIVATE);
+        // Iniciar database
+        database = FirebaseDatabase.getInstance();
 
         contenedor = findViewById(R.id.contenedorContactosMain);
         resource = R.layout.contacto_fila;
 
-        String listaCodificada = sharedPreferences.getString(Configuracion.L_contactos, null);
 
+
+        // String listaCodificada = sharedPreferences.getString(Configuracion.L_contactos, null);
+        /*
         if (listaCodificada == null){
             contactos = new ArrayList<>();
         }else{
             parser = new Gson();
             contactos = parser.fromJson(listaCodificada, new TypeToken< ArrayList<Contacto> >(){}.getType());
         }
+        */
+
+        // Que no explote al inicar con firebase
+        if (contactos == null){
+            contactos = new ArrayList<>();
+        }
+
+        // LECTURA EN FIREBASE
+        // Referencia BD Firebase
+        refContactos = database.getReference(Configuracion.REF_CONTACTOS);
+        // Cada vez que algo cambien en la BD, se lo trae, y repinta listaContactos
+        refContactos.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // SI el snapshot existe:
+                if (snapshot.exists()){
+                    // Genera un tipo genérico para codificar el arraylist de contactos
+                    GenericTypeIndicator<ArrayList<Contacto>> gtiContactos = new GenericTypeIndicator<ArrayList<Contacto>>() {};
+                    // Limpiamos la lista y le metemos el snapshot
+                    contactos.clear();
+                    contactos.addAll(snapshot.getValue(gtiContactos));
+                    // notificar los cambios al adapter
+                    contactosAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         contactosAdapter = new ContactosAdapter(this, resource, contactos);
         contenedor.setAdapter(contactosAdapter);
@@ -118,10 +165,15 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
             // Escribimos en las SharedPreferences
+            /*
             SharedPreferences.Editor editor = sharedPreferences.edit();
             String listaCodificada = new Gson().toJson(contactos);
             editor.putString(Configuracion.L_contactos, listaCodificada);
             editor.apply();
+             */
+
+            // ESCRIBIR EN FIREBASE
+            refContactos.setValue(contactos);
         }
     }
 
--- app/src/main/java/com/marcosledesma/agendacontactosedu/configuraciones/Configuracion.java
+++ app/src/main/java/com/marcosledesma/agendacontactosedu/configuraciones/Configuracion.java
@@ -3,4 +3,5 @@ package com.marcosledesma.agendacontactosedu.configuraciones;
 public class Configuracion {
     public static final String SP_LISTA = "lista";
     public static final String L_contactos = "contactos";
+    public static final String REF_CONTACTOS = "contactos";
 }
